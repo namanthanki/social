87b08c8e2f4f80b912ca5a3e304f2454520af9dc
Naman Thanki <stuniz7855@gmail.com>
2024-10-17 02:06:04 +0530

    changed to jsonResponse for consistency instead of writeJSON

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -5,12 +5,9 @@ import (
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Set("Content-Type", "application/json")
-	// w.WriteHeader(http.StatusOK)
-	// w.Write([]byte(`{"alive": true}`))
 	data := map[string]any{"alive": true, "env": app.config.env, "version": version}
 
-	if err := writeJSON(w, http.StatusOK, data); err != nil {
+	if err := app.jsonResponse(w, http.StatusOK, data); err != nil {
 		app.internalServerError(w, r, err)
 	}
 }
