4e829e8e6a0c64e5d131deb7cad0ed52a152b9ff
Naman Thanki <stuniz7855@gmail.com>
2024-10-17 01:02:13 +0530

    minor mistake fixed, password should be excluded from user model struct

--- internal/store/users.go
+++ internal/store/users.go
@@ -9,7 +9,7 @@ type User struct {
 	ID        int64  `json:"id"`
 	Username  string `json:"username"`
 	Email     string `json:"email"`
-	Password  string `json:"password"`
+	Password  string `json:"-"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 }
