729e484df34c8621919128d3a674bbc4498dd7f1
Naman Thanki <stuniz7855@gmail.com>
2024-10-17 02:06:55 +0530

    query timeout

--- internal/store/comments.go
+++ internal/store/comments.go
@@ -28,6 +28,9 @@ func (s *CommentStore) GetByPostID(ctx context.Context, postID int64) ([]Comment
 		ORDER BY c.created_at ASC
 	`
 
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
 	rows, err := s.db.QueryContext(ctx, query, postID)
 	if err != nil {
 		return nil, err
