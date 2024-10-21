3ba90bc7fb852c2dec91dc79e423a73dddc80be4
Naman Thanki <stuniz7855@gmail.com>
2024-10-21 23:50:37 +0530

    getUserFeedHandler to serve the users/:id/feed endpoint

---
+++ cmd/api/feed.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/namanthanki/social/internal/store"
+)
+
+func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
+	fq := store.PaginatedFeedQuery{
+		Limit:  10,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(3), fq)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, feed); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
